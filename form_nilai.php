c104592ef7587e9254b9c5c4917b2c1d20aeadcd
Muhamadindraikmaludin244 <muhamadindraikmaludin244@gmail.com>
2024-06-23 14:14:35 +0700

    praktikum2

---
+++ form_nilai.php
@@ -0,0 +1,50 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<form action="nilai_siswa.php" method="post">
+    <br>
+    <br>
+    <ol>
+  <div class="form-group row"> 
+    <label class="col-3 col-form-label" for="nama">Nama Lengkap</label> 
+    <div class="col-8">
+      <input id="nama" name="nama" placeholder="Nama Lengkap" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="Matkul" class="col-3 col-form-label">Mata Kuliah</label> 
+    <div class="col-8">
+      <select id="mmatkul" name="matkul" class="custom-select">
+        <option value="DDP">Dasar-Dasar Pemrograman</option>
+        <option value="BDI">Basis Data I</option>
+        <option value="WEB 1">Pemrograman Web</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nilai_uts" class="col-3 col-form-label">Nilai UTS</label> 
+    <div class="col-8">
+      <input id="nilai_uts" name="nilai_uts" placeholder="Nilai UTS" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nilai_uas" class="col-3 col-form-label">Nilai UAS</label> 
+    <div class="col-8">
+      <input id="nilai_uas" name="nilai_uas" placeholder="Nilai UAS" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nilai_tugas" class="col-3 col-form-label">Nilai Tugas/Praktikum</label> 
+    <div class="col-8">
+      <input id="nilai_tugas" name="nilai_tugas" placeholder="Nilai Tugas/Praktikum" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-3 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Simpan</button>
+    </div>
+  </div>
+</form>
+
+
+
